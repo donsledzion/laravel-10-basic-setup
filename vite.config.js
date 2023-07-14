3d0968697233f32d64f502d5acf774aeaaf26686
DESKTOP-K94QVJ7\choja <adam.chojaczyk@epicvr.pl>
2023-07-14 23:49:37 +0200

    Initial commit
    Changed public to public_html as defaul app directory. Installed jQuery, Sweetalert2 and Fontawesome(Fortawesome) libraries. Set default locale and timezone to PL, changed vite.js to deal with libraries.

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            publicDirectory: "public_html",
+            refresh: true,
+        }),
+    ],
+    resolve: {
+        alias: {
+            '$': 'jQuery'
+        },
+    },
+    server:{
+        hmr: {
+            host: 'localhost'
+        }
+    }
+
+});
