1d60617e85403d7109ad93d2175aba193b2f99f9
Adam Chojaczyk <adam.chojaczyk@epicvr.pl>
2024-08-11 15:00:03 +0200

    ...

--- app/Http/Controllers/ScenarioController.php
+++ app/Http/Controllers/ScenarioController.php
@@ -57,7 +57,7 @@ class ScenarioController extends Controller
                 if($logoFile == null || Str::emtpy($logoFile->logo)){
                     $logoFile = $scenario->organization->getLogoFile();
                 }*/
-                //$newScenario->logo = $scenario->getLogoFile();
+                $newScenario->logo = $scenario->getLogoFile($scenario->logo);
 
                 $quizzes = new Collection();
                 foreach($scenario->quizzes as $quiz)
